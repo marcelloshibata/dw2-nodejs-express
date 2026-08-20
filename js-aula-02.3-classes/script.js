7a6204a53d52fb940436232f84efbf0240a2b616
DSM2 <fatec-dsm2@academico.intra>
2026-08-20 19:32:42 -0300

    aula 02.3 classes começo

---
+++ js-aula-02.3-classes/script.js
@@ -0,0 +1,33 @@
+class Carro {
+    // Nome de classes devem iniciar com a primeira letra maiúscula.
+
+    // ATRIBUTOS
+    constructor(marca, modelo, ano) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.ano = ano;
+    }
+    // MÉTODOS
+    buzinar() {
+        return "Beep! Beep!";
+    }
+}
+
+// Criando uma instância (objeto) da classe Carro
+const carroPopular = new Carro("Fiat", "Uno", "2012");
+
+// Acessando os atributos e métodos do objeto
+document.write(`
+    <p>O carro popular ${carroPopular.marca} modelo ${carroPopular.modelo} é do ano 
+    ${carroPopular.ano} e quando ele buzina ele faz ${carroPopular.buzinar()}</p>
+    `);
+
+const carroEsportivo = new Carro();
+carroEsportivo.marca = "Ferrari";
+carroEsportivo.modelo = "Spider";
+carroEsportivo.ano = "2026";
+
+document.write(`
+    <p>O carro popular ${carroEsportivo.marca} modelo ${carroEsportivo.modelo} é do ano 
+    ${carroEsportivo.ano} e quando ele buzina ele faz ${carroEsportivo.buzinar()}</p>
+    `);
