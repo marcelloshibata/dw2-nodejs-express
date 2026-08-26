d35162dfa60f829ea68eb6103d4acaaa1699ecdf
DSM2 <fatec-dsm2@academico.intra>
2026-08-26 20:58:45 -0300

    inicio de exercicio

---
+++ js-aula-02.3-classes/exercicio-classes/script.js
@@ -0,0 +1,45 @@
+class Heroi {
+  constructor(nome, vida, velocidade, forca) {
+    this.nome = nome;
+    this.vida = vida;
+    this.velocidade = velocidade;
+    this.forca = forca;
+  }
+
+  correr() {
+    return `Correndo a ${this.velocidade}`;
+  }
+  andar() {
+    return `Andando a ${this.velocidade / 2}`;
+  }
+  atacar() {
+    return `O herói ataca!! Desfere um golpe de ${this.forca} de dano`;
+  }
+  defender() {
+    return `O herói defende!! Mitigando ${this.forca / 3} de dano`;
+  }
+}
+
+// Instanciando objetos representando os heróis
+const homemAranha = new Heroi('Homem-Aranha', 300, 100, 400);
+homemAranha.teia = 1;
+homemAranha.sentidoAranha() = () => {
+    return `Detectando perigo!`
+}
+
+const superman = new Heroi('Superman', 1500, 1800, 2000);
+superman.podeVoar = 1;
+superman.visaoCalor() = () => {
+    return `Usando visão de calor!`;
+}
+
+const batman = new Heroi('Batman', 500, 400, 250);
+batman.esconder = 1;
+batman.investigar() = () => {
+    return `Investigando um crime do Coringa!`;
+}
+
+document.write(`
+    O ${homemAranha.nome} está em ação! Ele está ${homemAranha.correr()}, e ele encontra o 
+    Dr. Octopus! ${homemAranha.atacar()}
+    `);
